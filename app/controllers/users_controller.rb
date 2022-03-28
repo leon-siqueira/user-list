afec434d1ca69bb543cac1c59747ec7144a42bc5
leon-siqueira <leon.siqueir4@gmail.com>
2022-03-28 19:53:10 -0300

    show and index done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @translated_gender = gender_transltate(@user.gender)
+  end
+
+  private
+
+  def gender_transltate(gender)
+    case gender
+    when 'male'
+      'Masculino'
+    when 'female'
+      'Feminino'
+    else
+      'Não especificado'
+    end
   end
 end
--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -1,2 +1,6 @@
 module IndexHelper
+  # alternates the color of the row
+  def alternate_color(count)
+    count.odd? ? 'bg-light' : 'bg-grey'
+  end
 end
