67a00040d7c0d7ffa7e488eba9f699011e685cad
leon-siqueira <leon.siqueir4@gmail.com>
2022-03-28 17:01:58 -0300

    user model created, active storage model created and setted for local storage, seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+request = JSON.parse(URI.open("https://randomuser.me/api/?format=json&results=30&inc=gender,name,email,Picture&nat=br&seed=giga").read)
+
+request['results'].each do |result|
+  user = User.new(name: "#{result['name']['first']} #{result['name']['last']}",
+                  gender: result['gender'],
+                  email: result['email'])
+  user.save
+  photo = URI.open(result['picture']['large'])
+  user.photo.attach(io: photo, filename: "#{user.id}.jpg", content_type: 'image/jpg')
+end
