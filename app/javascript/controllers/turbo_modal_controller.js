45073d7ca68e02625cdfb0618dc0947b2ef691a1
leon-siqueira <leon.siqueir4@gmail.com>
2022-04-02 16:14:32 -0300

    added animation to modal

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -2,11 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="turbo-modal"
 export default class extends Controller {
-  static targets = ["modal"]
-
-  connect() {
-    console.log('hello world!')
-  }
 
   hideModal() {
     this.element.parentElement.removeAttribute("src") // it might be nice to also remove the modal SRC
