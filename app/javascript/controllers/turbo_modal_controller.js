eb24b2de07c9eed3ede6898f627f268222d1791f
leon-siqueira <leon.siqueir4@gmail.com>
2022-04-04 17:52:27 -0300

    fixed bug that closes the modal by clicking anywhere, added the features of closing by clicking outside window or pressing ESC

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -3,8 +3,24 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="turbo-modal"
 export default class extends Controller {
 
+  static targets = ["modal", "window"]
+
   hideModal() {
-    this.element.parentElement.removeAttribute("src") // it might be nice to also remove the modal SRC
-    this.element.remove()
+    this.modalTarget.parentElement.removeAttribute("src") // it might be nice to also remove the modal SRC
+    this.modalTarget.remove()
+  }
+
+  closeWithKeyboard(e) {
+    console.log(e);
+    if (e.code == "Escape") {
+      this.hideModal()
+    }
+  }
+
+  closeBackground(e) {
+    if (e && this.windowTarget.contains(e.target)) {
+      return
+    }
+    this.hideModal()
   }
 }
